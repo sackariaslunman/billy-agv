f2ba5b019151fa054398ab39fe1f5127adc07a63
Sackarias Lunman <sackarias.lunman@gmail.com>
2022-09-22 11:21:22 +0200

    added comment describing X, Y and theta of the linear regression

--- simulation/dcmotor_regression.m
+++ simulation/dcmotor_regression.m
@@ -3,6 +3,14 @@
 % Va = i*R + L*di/dt + KePhi*w
 % Tm = KePhi*i = TL + J*dw/dt + B*w
 
+% Linear regression:
+% Y = [[ Va - KePhi*w]
+%      [ KePhi*i - TL ]]
+%
+% X = [[ i, di/dt, 0, 0]
+%      [ 0, 0, dw/dt, w]]  
+%
+% Theta = [ R, L, J, B ]
+
 T = readtable("dcmotor_simdata.xls");
 disp(T)
-
