5a93d2662c63e9bfba1bb95b293150583be4dae5
Sackarias Lunman <sackarias.lunman@gmail.com>
2022-10-01 10:18:06 +0200

    removed system file

--- simulation/dcmotor_parameters.m
+++ simulation/dcmotor_parameters.m
@@ -1,12 +1,12 @@
 Ra = 0.5;
 La = 0.1;
-J = 5;
-B = 0.01;
+Jm = 5;
+Bm = 0.01;
 TL = 20;
 Va = 12;
 tau_a = La/Ra;
-tau_m = J/B;
-KePhi = 1.6;
+tau_m = Jm/Bm;
+Ke = 1.6;
 
 % Ia = (Va - KePhi*W)/(Ra*(1+s*tau_a))
 
