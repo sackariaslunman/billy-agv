3a8c8a8bac57e26773360a1942b5d740d63c2322
Sackarias Lunman <sackarias.lunman@gmail.com>
2022-10-08 14:58:51 +0200

    folder restructuring

--- simulation/simulation/dcmotor_parameters.m
+++ simulation/simulation/dcmotor_parameters.m
@@ -1,7 +1,7 @@
-Ra = 0.5;
+Ra = 0.01;
 La = 0.1;
-Jm = 0.;
-Bm = 0.1;
+Jm = 0.02;
+Bm = 0.03;
 TL = 0;
 Va = 12;
 Ke = 0.02;
@@ -9,6 +9,6 @@ Ke = 0.02;
 % Ia = (Va - KePhi*W)/(Ra*(1+s*tau_a))
 
 T = table(out.dcmotor_simdata.time, out.dcmotor_simdata.signals.values);
-writetable(T, "./datasets/sim/dcmotor_simdata.xls");
+writetable(T, "./datasets/simulated/dcmotor_simdata.xls");
 
 disp(T)
