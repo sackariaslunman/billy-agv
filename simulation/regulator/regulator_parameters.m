3bed781948029f445a8d8d01dd0098145f1d43fe
Sackarias Lunman <sackarias.lunman@gmail.com>
2022-10-22 23:17:38 +0200

    added pid controller and simulation

--- simulation/regulator/regulator_parameters.m
+++ simulation/regulator/regulator_parameters.m
@@ -1,10 +1,31 @@
-Ra = 0.12;
-La = 0.00027;
+Ra = 0.11;
 Ke = 0.022;
+Jm = 8.6e-5;
+Bm = 7.9e-5;
 
-Jm = 8.5e-5;
-Bm = 8.8e-5;
+TL_max = 0.2;
+W_max = 100;
+Va_max = 12;
+Ia_max = 40;
 
-Va = 12;
+Kg = Ke / (Ra * Bm + Ke^2);
+Tg = (Ra * Jm) / (Ra * Bm + Ke^2);
 
-TL = 0;
+% Define regulator specifications
+W_G150 = sqrt(3) / Tg;
+disp(W_G150)
+
+syms tau
+Wc = 0.6 * W_G150;
+Phi_m = (50 / 180) * pi;
+zeta = 1;
+beta = 10;
+
+% Define regulator transfer function
+K_inf = Wc * tau * beta * sqrt(1 + (Wc*tau / beta)^2) / ((Kg / Wc * sqrt(1 + (Wc * Tg)^2)) * sqrt((1-(Wc*tau)^2)^2 + (2*zeta*Wc*tau)^2));
+
+Ki = round(Ki, 4);
+Ti = round(Ti, 4);
+
+disp(Ki)
+disp(Ti)
