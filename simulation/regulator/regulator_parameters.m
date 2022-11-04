d4fbb0cf6c12d496c535308c4c1dc43daffc55a8
Sackarias Lunman <sackarias.lunman@gmail.com>
2022-11-04 19:39:15 +0100

    added anti windup to regulator simulator

--- simulation/regulator/regulator_parameters.m
+++ simulation/regulator/regulator_parameters.m
@@ -6,10 +6,18 @@ Bm = 8.7e-5;
 TL_max = 0.2;
 W_max = 10;
 Va_max = 12;
-Ia_max = 40;
+Ia_max = 20;
 
 Kg = Ke / (Ra * Bm + Ke^2);
 Tg = (Ra * Jm) / (Ra * Bm + Ke^2);
 
-N_speed = 0.02;
-N_torque = 1 / N_speed;
+N_torque = 50;
+N_speed = 1 / N_torque;
+
+s = tf("s");
+G = N_speed * Kg / (s * (1 + Tg * s));
+
+Ts = 0.001;
+Gd = c2d(G, Ts);
+[Fd_pid, info] = pidtune(Gd, "PIDF");
+Td_pid = feedback(Fd_pid * Gd, 1);
