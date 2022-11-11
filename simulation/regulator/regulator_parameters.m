ce0bd05bd840c1d8a39da500530c1877ef701b27
Sackarias Lunman <sackarias.lunman@gmail.com>
2022-11-12 00:14:30 +0100

    jag lade till kuggväxlingen och nu funkar inget. optimeringen hittar inga bra värden, jag är lost. Kan vara att processöverföringen är fel, ska undersöka

--- simulation/regulator/regulator_parameters.m
+++ simulation/regulator/regulator_parameters.m
@@ -8,31 +8,34 @@ W_max = 10;
 Va_max = 12;
 Ia_max = 10;
 
-Kg = Ke / (Ra * Bm + Ke^2);
-Tg = (Ra * Jm) / (Ra * Bm + Ke^2);
-
 N_torque = 50;
 N_speed = 1 / N_torque;
 
+Kg = N_speed * Ke / (Ra * Bm + Ke^2);
+Tg = (Ra * Jm) / (Ra * Bm + Ke^2);
+
 Phi_m = 50 * (pi / 180);
 W_G150 = sqrt(3) / Tg;
-Wc = 0.6 * W_G150;
-zeta = 1;
-beta = 10;
 
-tau = 0.5 / Wc;
+Wc = 30;
+
+tau = 1.476e+4;
+zeta = 0.1;
+beta = 1.04e+3;
 
 G_abs = Kg / (Wc * sqrt(1 + (Tg*Wc)^2));
-K_inf = Wc * tau * beta * sqrt(1 + (Wc*tau/beta)^2) / (G_abs * sqrt((1 - (Wc*tau)^2)^2 ) + (2*zeta*Wc*tau)^2);
+K_inf = Wc * tau * beta * sqrt(1 + (Wc*tau/beta)^2) / (G_abs * sqrt(((1 - (Wc*tau)^2)^2 ) + (2*zeta*Wc*tau)^2) );
 
 Ki = K_inf / (tau * beta);
 Tf = tau / beta;
 Ti = 2*zeta*tau - Tf;
 Td = (tau^2)/Ti - Tf;
 Kp = Ki * Ti;
+Kd = Kp * Td;
 
-% syms s K z t b
-% F_pid = K * (1 + 2*z*s*t + (s*t)^2) / (s * (1 + s*t/b));
-% F_pid_inf = K*t*b;
-% Fs_pid = simplify(1 - F_pid_inf / F_pid);
-% disp(Fs_pid)
+Kp = 70 / 2;
+Ki = Kp / 2;
+Kd = Ki / 2;
+disp(Kp)
+disp(Ki)
+disp(Kd)
